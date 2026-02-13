73dab84c8d7a36a9ffd448f4fd51507c0424f01b
Ziad Skafi <ziad.skafi12@gmail.com>
2026-02-13 15:30:51 +0400

    separated tree-sitter

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,31 +1,33 @@
-; Keywords
-"struct" @keyword
-"fun" @keyword
-
-; Types
-(type_identifier) @type
-
-(struct_decl
-  name: (type_identifier) @type.definition)
-
-; Functions
-(function_decl
-  name: (identifier) @function.definition)
-
-(call_expression
-  function: (identifier) @function.call)
-
-; Variables
 (identifier) @variable
 
-(member_expression
-  property: (identifier) @variable.other.member)
+; ; Keywords
+; "struct" @keyword
+; "fun" @keyword
 
-; Literals
-(string) @string
-(number) @number
-(boolean) @constant.builtin
-(nil) @constant.builtin
+; ; Types
+; (type_identifier) @type
 
-; Comments
-(comment) @comment
+; (struct_decl
+;   name: (type_identifier) @type.definition)
+
+; ; Functions
+; (function_decl
+;   name: (identifier) @function.definition)
+
+; (call_expression
+;   function: (identifier) @function.call)
+
+; ; Variables
+; (identifier) @variable
+
+; (member_expression
+;   property: (identifier) @variable.other.member)
+
+; ; Literals
+; (string) @string
+; (number) @number
+; (boolean) @constant.builtin
+; (nil) @constant.builtin
+
+; ; Comments
+; (comment) @comment
